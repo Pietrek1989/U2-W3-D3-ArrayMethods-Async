e791a4654ff73d2e92675fb9759eeb39ea129b23
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-01-18 20:18:06 +0100

    added search function, counter of cart, remove items from cart

--- script.js
+++ script.js
@@ -1,50 +1,56 @@
 let linkBooks = "https://striveschool-api.herokuapp.com/books";
-
 const getData = async () => {
   try {
     let resultRaw = await fetch(linkBooks);
     let result = await resultRaw.json();
-    console.log(result);
-    let container = document.querySelector("#row");
-
-    result.forEach((book) => {
-      container.innerHTML += `
-          <div class="col-lg-3 col-md-4 col-sm-6">
-              <div class="card">
-                  <img class="card-img-top" src="${book.img}" alt="picture">
-                  <div class="card-body">
-                  <h5 class="card-title text-center">${book.title}</h5>
-                  <div class="btn-group">
-                  <button
-                    type="button"
-                    class="btn btn-sm btn-outline-secondary addBtn")
-                  >
-                    ADD TO CART
-                  </button>
-                  <button  type="button"
-                    class="btn btn-sm btn-outline-secondary removeBTN"
-                  >
-                    skip
-                  </div>
-              </div>
-              </div>
-          `;
-    });
+    outerBooks = result;
+    renderBooks(result);
     addToList(result);
     removeCard();
   } catch (error) {
     console.log(error);
   }
 };
+const renderBooks = (result) => {
+  let container = document.querySelector("#row");
+  container.innerHTML = "";
+  result.forEach((book) => {
+    container.innerHTML += `
+      <div class="col-lg-3 col-md-4 col-sm-6">
+          <div class="card">
+              <img class="card-img-top" src="${book.img}" alt="picture">
+              <div class="card-body">
+              <h5 class="card-title text-center title" >${book.title}</h5>
+              <div class="btn-group">
+              <button
+                type="button"
+                class="btn btn-sm btn-outline-secondary addBtn")
+              >
+                ADD TO CART
+              </button>
+              <button  type="button"
+                class="btn btn-sm btn-outline-secondary removeBTN"
+              >
+                skip
+              </div>
+          </div>
+          </div>
+      `;
+  });
+};
 
 const addToList = (book) => {
   const addBTN = document.querySelectorAll(".addBtn");
   const list = document.querySelector("#drop-list");
+  let counter = 0;
   for (let i = 0; i < addBTN.length; i++) {
     const element = addBTN[i];
     element.addEventListener("click", () => {
       element.classList.add("btn-success");
-      list.innerHTML += `<li>${book[i].title}</li>`;
+      list.innerHTML += `<li>${book[i].title}</li><button class="btn btn-danger" onclick="removeItem(event)">remove</button>`;
+      counter++;
+      const counterContainer = document.getElementById("counter");
+      counterContainer.innerHTML = `<p>The items in the cart is:${counter}</p>`;
     });
   }
 };
@@ -58,18 +64,32 @@ const removeCard = () => {
   }
 };
 
-function search(event) {
-  console.log(event);
-  // let characters = event.value.split();
-  const result = event.value.filter(() => word.length > 3);
-  console.log(result);
+function search(query) {
+  let cards = document.querySelectorAll(".card-title");
+  if (query.length > 3) {
+    const array = Array.prototype.slice.call(cards);
+    for (let i = 0; i < array.length; i++) {
+      const element = array[i];
+      if (element.innerText.toLowerCase().includes(query)) {
+        element.classList.add("bg-info");
+      } else {
+        element.parentElement.parentElement.remove();
+      }
+    }
+  } else if (query.length === 0) {
+    getData();
+  }
 }
-// if (characters > 3) {
-//   let searchWindow = document.querySelector(".form-control");
-//   let value = searchWindow.value;
-//   let searchButton = document.querySelector(".search-button");
-//   searchButton.addEventListener("click", getData(value));
-// }
 
-window.onload = getData();
-window.onload = search();
+const removeItem = (event) => {
+  event.target.parentElement.remove();
+};
+
+const removeAll = () => {
+  const list = document.querySelector("#drop-list");
+  list.innerHTML = "";
+};
+
+window.onload = () => {
+  getData();
+};
